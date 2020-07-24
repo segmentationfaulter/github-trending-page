33b8fed9e5bddfc7854d2a1f5a2f42a39ff5161d
segmentationfaulter <muhammadsackib@gmail.com>
2020-07-24 17:10:39 +0500

    create placeholder for trendingViewControls

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,11 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Browser
-import Element as El exposing (Element, column, el, row, text)
+import Element as El exposing (Element, column, el, fillPortion, row, text)
 import Element.Background as Background
+import Element.Border as Border
 import Element.Font as Font
-import Element.Region exposing (heading)
+import Element.Region exposing (heading, mainContent)
 import Html exposing (Html)
 
 
@@ -40,11 +41,23 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    El.layout [] header
+    El.layout [] elmUIView
 
 
-header : Element Msg
-header =
+elmUIView : Element Msg
+elmUIView =
+    column
+        [ mainContent
+        , El.width El.fill
+        , El.spacing 40
+        ]
+        [ headerView
+        , trendingView
+        ]
+
+
+headerView : Element Msg
+headerView =
     el
         [ El.width El.fill, El.height <| El.px 174, Background.color <| El.rgb255 250 251 252 ]
     <|
@@ -53,6 +66,31 @@ header =
             [ el [ El.centerX, heading 1, Font.size 40 ] <| text "Trending"
             , El.paragraph [ El.centerX ] [ text "See what the GitHub community is most excited about today." ]
             ]
+
+
+trendingView : Element Msg
+trendingView =
+    row
+        [ El.width El.fill
+        ]
+        [ el [ El.width <| fillPortion 1 ] El.none
+        , el
+            [ El.width <| fillPortion 3
+            ]
+            trendingViewControls
+        , el [ El.width <| fillPortion 1 ] El.none
+        ]
+
+
+trendingViewControls : Element Msg
+trendingViewControls =
+    row
+        [ Border.width 1
+        , Border.color <| El.rgb255 225 228 232
+        , Border.roundEach {topLeft = 6, topRight = 6, bottomLeft = 0, bottomRight = 0}
+        , El.width El.fill
+        ]
+        [ text "contorls view" ]
 
 
 

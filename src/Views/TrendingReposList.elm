43ce4790e70227cb481c82951b65e7d1727f07d5
segmentationfaulter <muhammadsackib@gmail.com>
2020-07-25 12:43:07 +0500

    format

--- src/Views/TrendingReposList.elm
+++ src/Views/TrendingReposList.elm
@@ -1,5 +1,7 @@
-module Views.TrendingReposList exposing(TrendingRepo, TrendingReposList, trendingReposListDecoder)
+module Views.TrendingReposList exposing (TrendingRepo, TrendingReposList, trendingReposListDecoder)
+
 import Json.Decode as Decode
+
 
 type alias TrendingRepo =
     { author : String
@@ -13,7 +15,10 @@ type alias TrendingRepo =
     , starsToday : Int
     }
 
-type alias TrendingReposList = List TrendingRepo
+
+type alias TrendingReposList =
+    List TrendingRepo
+
 
 trendingReposListDecoder : Decode.Decoder TrendingReposList
 trendingReposListDecoder =
@@ -21,22 +26,44 @@ trendingReposListDecoder =
         trendingRepoDecoder : Decode.Decoder TrendingRepo
         trendingRepoDecoder =
             let
-                authorDecoder = Decode.field "author" Decode.string
-                nameDecoder = Decode.field "name" Decode.string
-                urlDecoder = Decode.field "url" Decode.string
-                descriptionDecoder = Decode.field "description" Decode.string
-                languageDecoder = Decode.field "language" Decode.string
-                languageColorDecoder = Decode.field "languageColor" Decode.string
-                starsDecoder = Decode.field "stars" Decode.int
-                forksDecoder = Decode.field "forks" Decode.int
-                starsTodayDecoder = Decode.field "currentPeriodStars" Decode.int
+                authorDecoder =
+                    Decode.field "author" Decode.string
+
+                nameDecoder =
+                    Decode.field "name" Decode.string
+
+                urlDecoder =
+                    Decode.field "url" Decode.string
+
+                descriptionDecoder =
+                    Decode.field "description" Decode.string
+
+                languageDecoder =
+                    Decode.field "language" Decode.string
+
+                languageColorDecoder =
+                    Decode.field "languageColor" Decode.string
+
+                starsDecoder =
+                    Decode.field "stars" Decode.int
+
+                forksDecoder =
+                    Decode.field "forks" Decode.int
+
+                starsTodayDecoder =
+                    Decode.field "currentPeriodStars" Decode.int
             in
-                Decode.map2
-                    (\a b -> { author = a.uthor, name = a.name, url = a.url, description = a.description, language = a.language, languageColor = a.languageColor})
-                    Decode.map6
-                        (\author name url description language languageColor -> { author = author, name = name, url = url, description = description, language = language, languageColor = languageColor})
-                        authorDecoder nameDecoder urlDecoder descriptionDecoder languageDecoder languageColorDecoder 
-                    Decode.map3
-                        (\stars forks starsToday -> {stars = stars, forks = forks, starsToday = starsToday })
-        in
-            Decode.list trendingRepoDecoder
+            Decode.map2
+                (\a b -> { author = a.uthor, name = a.name, url = a.url, description = a.description, language = a.language, languageColor = a.languageColor })
+                Decode.map6
+                (\author name url description language languageColor -> { author = author, name = name, url = url, description = description, language = language, languageColor = languageColor })
+                authorDecoder
+                nameDecoder
+                urlDecoder
+                descriptionDecoder
+                languageDecoder
+                languageColorDecoder
+                Decode.map3
+                (\stars forks starsToday -> { stars = stars, forks = forks, starsToday = starsToday })
+    in
+    Decode.list trendingRepoDecoder

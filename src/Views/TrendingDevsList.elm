201418836a8dc5a0ded6f12b415d4894258e6225
segmentationfaulter <muhammadsackib@gmail.com>
2020-07-27 12:21:58 +0500

    decoder for trending devs

---
+++ src/Views/TrendingDevsList.elm
@@ -0,0 +1,62 @@
+module Views.TrendingDevsList exposing (TrendingDevsList)
+
+import Json.Decode as Decode
+
+
+type alias TrendingDev =
+    { username : String
+    , name : String
+    , url : String
+    , sponsorUrl : Maybe String
+    , avatar : String
+    , repo : PopularRepo
+    }
+
+
+type alias PopularRepo =
+    { name : String
+    , description : String
+    , url : String
+    }
+
+
+type alias TrendingDevsList =
+    List TrendingDev
+
+
+trendingDevsListDecoder : Decode.Decoder TrendingDevsList
+trendingDevsListDecoder =
+    let
+        usernameDecoder =
+            Decode.field "username" Decode.string
+
+        nameDecoder =
+            Decode.field "name" Decode.string
+
+        urlDecoder =
+            Decode.field "url" Decode.string
+
+        sponsorUrlDecoder =
+            Decode.maybe <| Decode.field "sponsorUrl" Decode.string
+
+        avatarDecoder =
+            Decode.field "avatar" Decode.string
+
+        repoDecoder : Decode.Decoder PopularRepo
+        repoDecoder =
+            Decode.field "repo"
+                (Decode.map3
+                    (\name description url -> { name = name, description = description, url = url })
+                    (Decode.field "name" Decode.string)
+                    (Decode.field "description" Decode.string)
+                    (Decode.field "url" Decode.string)
+                )
+    in
+    Decode.list <| Decode.map6
+        (\username name url sponsorUrl avatar repo -> { username = username, name = name, url = url, sponsorUrl = sponsorUrl, avatar = avatar, repo = repo })
+        usernameDecoder
+        nameDecoder
+        urlDecoder
+        sponsorUrlDecoder
+        avatarDecoder
+        repoDecoder
